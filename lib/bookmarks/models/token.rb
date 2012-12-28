337d4407fc9e6f156ae82d4a8396184c33ef5e29
Daniel Strittmatter <daniel@stritty.de>
2012-12-28 11:53:21 +0100

    Delete trailing spaces

--- lib/bookmarks/models/token.rb
+++ lib/bookmarks/models/token.rb
@@ -5,7 +5,7 @@ module Bookmarks
 		before_create :generate_key
 
 		def generate_key
-			chars = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a			
+			chars = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
 			result = ''
 			40.times do
 				result << chars[rand(chars.size)]
